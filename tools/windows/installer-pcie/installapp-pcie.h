6b3e208dea4a56c606551d6e4b030b02059a7e3c
Gaurav Kumar Singh <gaurav@kalycito.com>
2014-12-15 16:35:07 +0100

    [FEATURE] Add Windows driver installer and uinstaller applications
    - Add installer and uninstaller application for PCIe driver.
    - Add NSIS script to prepare openPOWERLINK installer.
    - Update Windows driver project to add install path.

---
+++ tools/windows/installer-pcie/installapp-pcie.h
@@ -0,0 +1,88 @@
+/**
+********************************************************************************
+\file   installapp-pcie.h
+
+\brief  Installation application for Windows driver - Header
+
+This file contains the definitions and prototypes required for installation
+application to install a PCIe driver on Windows system.
+*******************************************************************************/
+
+/*------------------------------------------------------------------------------
+Copyright (c) 2014, Kalycito Infotech Private Limited
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the name of the copyright holders nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL COPYRIGHT HOLDERS BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+------------------------------------------------------------------------------*/
+
+#ifndef _INC_installapp_pcie_H_ 
+#define _INC_installapp_pcie_H_
+
+//------------------------------------------------------------------------------
+// includes
+//------------------------------------------------------------------------------
+#include <cfgmgr32.h>
+#include <stdio.h>
+#include <tchar.h>
+#include <string.h>
+#include <newdev.h>
+//------------------------------------------------------------------------------
+// const defines
+//------------------------------------------------------------------------------
+#define OPLK_INF_NAME              "drv_ndis_pcie.inf"
+#define OPLK_DEVICE_ID             "PCI\\VEN_1677&DEV_E53F"
+
+#define UPDATEDRIVERFORPLUGANDPLAYDEVICES "UpdateDriverForPlugAndPlayDevicesA"
+
+#ifndef NDEBUG
+#define TRACE(...)    printf(__VA_ARGS__)
+#else
+#define TRACE(...)
+#endif
+
+//------------------------------------------------------------------------------
+// typedef
+//------------------------------------------------------------------------------
+//
+// UpdateDriverForPlugAndPlayDevices
+//
+typedef BOOL (WINAPI *UpdateDriverForPlugAndPlayDevicesProto)(__in HWND hwndParent,
+                                                              __in LPCTSTR HardwareId,
+                                                              __in LPCTSTR FullInfPath,
+                                                              __in DWORD InstallFlags,
+                                                              __out_opt PBOOL bRebootRequired
+                                                         );
+
+//------------------------------------------------------------------------------
+// function prototypes
+//------------------------------------------------------------------------------
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _INC_installapp_pcie_H_ */
---
+++ tools/windows/uninstaller-pcie/uninstallapp-pcie.h
@@ -0,0 +1,88 @@
+/**
+********************************************************************************
+\file   uninstallapp-pci.h
+
+\brief  Uninstaller application for Windows PCIe driver - Header
+
+This file contains the prototypes and definitions for Windows PCIe driver
+uninstall application.
+*******************************************************************************/
+
+/*------------------------------------------------------------------------------
+Copyright (c) 2014, Kalycito Infotech Private Limited
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the name of the copyright holders nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL COPYRIGHT HOLDERS BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+------------------------------------------------------------------------------*/
+
+#ifndef _INC_uninstallapp_pci_H_
+#define _INC_uninstallapp_pci_H_
+
+//------------------------------------------------------------------------------
+// includes
+//------------------------------------------------------------------------------
+#include <cfgmgr32.h>
+#include <stdio.h>
+#include <tchar.h>
+#include <strsafe.h>
+#include <setupapi.h>
+#include <string.h>
+#include <infstr.h>
+//------------------------------------------------------------------------------
+// const defines
+//------------------------------------------------------------------------------
+#define OPLK_INF_NAME          "drv_ndis_pcie.inf"
+#define OPLK_DEVICE_ID         "PCI\\VEN_1677&DEV_E53F"
+#define OPLK_PROVIDER_STRING   "Kalycito Infotech Private Limited"
+#define OPLK_FLAG_FORCE         0x00000001
+
+#define UPDATEDRIVERFORPLUGANDPLAYDEVICES "UpdateDriverForPlugAndPlayDevicesA"
+#define SETUPUNINSTALLOEMINF "SetupUninstallOEMInfA"
+
+#ifndef NDEBUG
+#define TRACE(...)    printf(__VA_ARGS__)
+#else
+#define TRACE(...)
+#endif
+//------------------------------------------------------------------------------
+// typedef
+//------------------------------------------------------------------------------
+typedef BOOL (WINAPI *SetupUninstallOEMInfProto)(__in LPCTSTR InfFileName,
+                                                 __in DWORD Flags,
+                                                 __reserved PVOID Reserved
+                                                 );
+
+
+//------------------------------------------------------------------------------
+// function prototypes
+//------------------------------------------------------------------------------
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _INC_uninstallapp_pci_H_ */
