7a41a0cd39429a0b9ce160df944e29e4d9128245
Kishore Kumar <kishorekumar.k@kalycito.com>
2015-03-06 16:26:24 +0530

    1.pll with output three clocks 50,100,125mhz 2.Removed the sram and dma clock crossing bridges in the toplevel qsys 3.toplevel.vhd clean up 4.Removed the clocking bridges

--- hardware/boards/terasic-de2i-150/mn-single-dualproc_shm-drv/quartus/toplevel.vhd
+++ hardware/boards/terasic-de2i-150/mn-single-dualproc_shm-drv/quartus/toplevel.vhd
@@ -53,34 +53,9 @@ use libcommon.global.all;
 
 entity toplevel is
     port (
-         -----------CLOCK2-------------
-         CLOCK2_50          : in    std_logic;
-        --------- CLOCK3 ---------
-         CLOCK3_50          : in    std_logic;
         --------- CLOCK ---------
         --! External 50Mhz Clock input
          CLOCK_50           : in    std_logic;
-        --------- DRAM ---------
-        --! SDRAM Address
-        DRAM_ADDR           : out    std_logic_vector (12 downto 0);
-        --! SDRAM Bank select
-        DRAM_BA             : out    std_logic_vector (1 downto 0);
-        --! SDRAM active low CAS (Column Address Select)
-        DRAM_CAS_N          : out    std_logic;
-        --! SDRAM Clock enable
-        DRAM_CKE            : out    std_logic;
-        --! SDRAM Clock
-        DRAM_CLK            : out    std_logic;
-        --! SDRAM Active low chip select
-        DRAM_CS_N           : out    std_logic;
-        --! SDRAM Data
-        DRAM_DQ             : inout    std_logic_vector (31 downto 0);
-        --! SDRAM Data Mask
-        DRAM_DQM            : out    std_logic_vector (3 downto 0);
-        --! SDRAM active low RAS (Row Address Select)
-        DRAM_RAS_N          : out    std_logic;
-        --! SDRAM active low Write Enable
-        DRAM_WE_N           : out    std_logic;
         --------- EEP ---------
         --! I2C Clock for EEPROM
         EEP_I2C_SCLK        : out   std_logic;
@@ -139,7 +114,7 @@ entity toplevel is
         --! Flash-SSRAM Data
         FS_DQ               : inout std_logic_vector (31 downto 0);
         --! Flash-SSRAN address
-        FS_ADDR             : out   std_logic_vector (25 downto 0); -- 26th pin is not used by S29GL512S FLASH
+        FS_ADDR             : out   std_logic_vector (21 downto 0);
         --------- GPIO ---------
         GPIO                : inout std_logic_vector (35 downto 0);
         --------- G ---------
@@ -163,18 +138,14 @@ entity toplevel is
         HEX6         : out   std_logic_vector (6 downto 0);
         --! Seven Segment Display -8
         HEX7         : out   std_logic_vector (6 downto 0);
-
         --------- I2C ---------
         I2C_SCLK     : out   std_logic;
         I2C_SDAT     : inout std_logic;
-
         --------- IRDA ---------
         IRDA_RXD     : in    std_logic;
-
         --------- KEY ---------
         --! Key input
         KEY  :   in     std_logic_vector (3 downto 0);
-
         --------- LCD ---------
         --! LCD data
         LCD_DATA     :   inout   std_logic_vector (7 downto 0);
@@ -192,7 +163,6 @@ entity toplevel is
         --------- LEDR ---------
         --! LED Red
         LEDR : out   std_logic_vector (17 downto 0);
-
         --------- PCIE ---------
         --! PCIE Active low PReset
         PCIE_PERST_N     :   in  std_logic;
@@ -204,23 +174,11 @@ entity toplevel is
         PCIE_TX_P        :   out  std_logic_vector (0 downto 0);
         --! PCIE Wakeup
         PCIE_WAKE_N      :   out std_logic;
-
-        --------- SD ---------
-        --! SD Clock
-        SD_CLK       : out       std_logic;
-        --! SD Command
-        SD_CMD       : inout     std_logic;
-        --! SD data
-        SD_DAT       : inout     std_logic_vector (3 downto 0);
-        --! SD WP
-        SD_WP_N      : in        std_logic;
-
         --------- SMA ---------
         --! SMA Clock in
         SMA_CLKIN    :   in      std_logic;
         --! SMA Clock out
         SMA_CLKOUT   :   out     std_logic;
-
         --------- SSRAM ---------
         --! SSRAM active low ADSC
         SSRAM_ADSC_N     : out   std_logic_vector(0 downto 0);
@@ -240,19 +198,14 @@ entity toplevel is
         SSRAM_WE_N       : out   std_logic_vector(0 downto 0);
         --! SSRAM-0 Chip Enable
         SSRAM0_CE_N      : out   std_logic_vector(0 downto 0);
-        --! SSRAM-1 Chip Enable
-        SSRAM1_CE_N      : out   std_logic_vector(0 downto 0);
-
         --------- SW ---------
         SW               : out  std_logic_vector (17 downto 0);
-
         --------- TD --------
         TD_CLK27     : in    std_logic;
         TD_DATA      : in std_logic_vector (7 downto 0);
         TD_HS        : in std_logic;
         TD_RESET_N   : out std_logic;
         TD_VS        : in std_logic;
-
         --------- UART ---------
         UART_CTS     : in        std_logic;
         UART_RTS     : out       std_logic;
@@ -274,21 +227,11 @@ signal ledRG_b              : std_logic_vector (31 downto 0) ;
 
 signal clk50                : std_logic;
 signal clk100               : std_logic;
-signal clk25                : std_logic;
-signal clk150p              : std_logic;
-signal clk150              : std_logic;
+signal clk125               : std_logic;
 signal pllLocked            : std_logic;
 
 signal flash_reset_n        : std_logic_vector(0 downto 0);
 signal nios_flash_reset_n   : std_logic;
-
-     --! Heart Beat Module
-     component heart_beat
-        port (
-            clk     : in std_logic;
-            led     : out std_logic
-          );
-      end component heart_beat;
 
     --! PLL component
     component pll
@@ -297,8 +240,6 @@ signal nios_flash_reset_n   : std_logic;
             c0      : out std_logic;
             c1      : out std_logic;
             c2      : out std_logic;
-            c3      : out std_logic;
-            c4      : out std_logic;
             locked  : out std_logic
         );
     end component pll;
@@ -307,17 +248,9 @@ signal nios_flash_reset_n   : std_logic;
     component mnSingleDualProcDrv is
         port (
             clk_clk                                    : in    std_logic                     := 'X';             -- clk
-            reset_reset_n                              : in    std_logic                     := 'X';             -- reset_n
             clk_100_clk                                : in    std_logic                     := 'X';             -- clk
---            sdram_addr                                 : out   std_logic_vector(12 downto 0);                    -- addr
---            sdram_ba                                   : out   std_logic_vector(1 downto 0);                     -- ba
---            sdram_cas_n                                : out   std_logic;                                        -- cas_n
---            sdram_cke                                  : out   std_logic;                                        -- cke
---            sdram_cs_n                                 : out   std_logic;                                        -- cs_n
---            sdram_dq                                   : inout std_logic_vector(31 downto 0) := (others => 'X'); -- dq
---            sdram_dqm                                  : out   std_logic_vector(3 downto 0);                     -- dqm
---            sdram_ras_n                                : out   std_logic;                                        -- ras_n
---            sdram_we_n                                 : out   std_logic;                                        -- we_n
+            clk_125_clk                                : in    std_logic                     := 'X';           -- clk
+            reset_reset_n                              : in    std_logic                     := 'X';             -- reset_n
             led_external_connection_export             : out   std_logic_vector(20 downto 0);                    -- export
             button_external_connection_export          : in    std_logic_vector(3 downto 0)  := (others => 'X'); -- export
             pcie_ip_reconfig_togxb_data                : in    std_logic_vector(3 downto 0)  := (others => 'X'); -- data
@@ -346,19 +279,17 @@ signal nios_flash_reset_n   : std_logic;
             pcie_ip_rx_in_rx_datain_0                  : in    std_logic                     := 'X';             -- rx_datain_0
             pcie_ip_tx_out_tx_dataout_0                : out   std_logic;                                        -- tx_dataout_0
             pcie_ip_reconfig_fromgxb_0_data            : out   std_logic_vector(4 downto 0);                     -- data
-            --tristate_conduit_bridge_0_out_SSRAM_1_CS_N : out   std_logic_vector(0 downto 0);                     -- SSRAM_1_CS_N
-            tristate_conduit_bridge_0_out_DATA         : inout std_logic_vector(31 downto 0) := (others => 'X'); -- DATA
-            tristate_conduit_bridge_0_out_SSRAM_BE_N   : out   std_logic_vector(3 downto 0);                     -- SSRAM_BE_N
-            --tristate_conduit_bridge_0_out_CFI_0_WR_N   : out   std_logic_vector(0 downto 0);                     -- CFI_0_WR_N
-            tristate_conduit_bridge_0_out_ADDR         : out   std_logic_vector(25 downto 0);                    -- ADDR
-            tristate_conduit_bridge_0_out_SSRAM_BT_N   : out   std_logic_vector(0 downto 0);                     -- SSRAM_BT_N
-            tristate_conduit_bridge_0_out_SSRAM_WR_N   : out   std_logic_vector(0 downto 0);                     -- SSRAM_WR_N
-            --tristate_conduit_bridge_0_out_CFI_0_OE_N   : out   std_logic_vector(0 downto 0);                     -- CFI_0_OE_N
-            tristate_conduit_bridge_0_out_SSRAM_0_CS_N : out   std_logic_vector(0 downto 0);                     -- SSRAM_0_CS_N
-            --tristate_conduit_bridge_0_out_CFI_0_CS_N   : out   std_logic_vector(0 downto 0);                     -- CFI_0_CS_N
-            tristate_conduit_bridge_0_out_SSRAM_OE_N   : out   std_logic_vector(0 downto 0);                     -- SSRAM_OE_N
-            --tristate_conduit_bridge_0_out_CFI_0_RST_N  : out   std_logic_vector(0 downto 0);                     -- CFI_0_RST_N
-            flash_reset_n_export                       : out   std_logic;                                        -- export
+            pcie_ip_powerdown_pll_powerdown            : in    std_logic                     := 'X';             -- pll_powerdown
+            pcie_ip_powerdown_gxb_powerdown            : in    std_logic                     := 'X';              -- gxb_powerdown
+            pcp_0_cpu_resetrequest_resetrequest        : in    std_logic                     := 'X';             -- resetrequest
+            pcp_0_cpu_resetrequest_resettaken          : out   std_logic;                                        -- resettaken
+            tri_state_0_tcm_data_out                   : inout std_logic_vector(31 downto 0) := (others => 'X'); -- DATA
+            tri_state_0_tcm_byteenable_n_out           : out   std_logic_vector(3 downto 0);                     -- SSRAM_BE_N
+            tri_state_0_tcm_address_out                : out   std_logic_vector(21 downto 0);                    -- ADDR
+            tri_state_0_tcm_begintransfer_n_out        : out   std_logic_vector(0 downto 0);                     -- SSRAM_BT_N
+            tri_state_0_tcm_write_n_out                : out   std_logic_vector(0 downto 0);                     -- SSRAM_WR_N
+            tri_state_0_tcm_chipselect_n_out           : out   std_logic_vector(0 downto 0);                     -- SSRAM_0_CS_N
+            tri_state_0_tcm_outputenable_n_out         : out   std_logic_vector(0 downto 0);                     -- SSRAM_OE_N                           
             openmac_mii_txEnable                       : out   std_logic_vector(0 downto 0);                     -- txEnable
             openmac_mii_txData                         : out   std_logic_vector(3 downto 0);                     -- txData
             openmac_mii_txClk                          : in    std_logic_vector(0 downto 0)  := (others => 'X'); -- txClk
@@ -369,19 +300,16 @@ signal nios_flash_reset_n   : std_logic;
             openmac_smi_nPhyRst                        : out   std_logic_vector(0 downto 0);                     -- nPhyRst
             openmac_smi_clk                            : out   std_logic_vector(0 downto 0);                     -- clk
             openmac_smi_dio                            : inout std_logic_vector(0 downto 0)  := (others => 'X'); -- dio
-            clk_150_clk                                : in    std_logic                     := 'X';             -- clk
             benchmark_pio_export                       : out   std_logic_vector(7 downto 0)
-				--host_benchmark_pio_export                                     : out   std_logic_vector(7 downto 0) -- export
-				);
+            );
     end component mnSingleDualProcDrv;
 
 begin
 
     -- PIN Declarations
     FL_RESET_N             <= flash_reset_n(0) and nios_flash_reset_n;
-    DRAM_CLK               <= clk150;
-    SSRAM_CLK              <= clk150;
-    PCIE_WAKE_N            <= cnInactivated;   -- 07/30/2013, pull-high to avoid system reboot after power off
+    SSRAM_CLK              <= clk100;
+    PCIE_WAKE_N            <= cnInactivated; 
     FL_WP_N                <= cnInactivated;
     SSRAM_GW_N             <= cnInactivated;
     SSRAM_ADSP_N           <= cnInactivated;
@@ -394,47 +322,53 @@ begin
     LEDG(3 downto 0)       <= b"1111";
     ENET_TX_ER             <= cInactivated;
     ENET_GTX_CLK           <= cInactivated;
-
-
--- Internal Signals
--- TODO: Can be avoid if needed by direct using
-    reset_n         <= pllLocked;
+    reset_n                <= pllLocked;
 
     --! Qsys module Instantiation
     inst : component  mnSingleDualProcDrv
              port map (
                 clk_clk                                    => clk50,
-                clk_150_clk                                => clk150,
                 clk_100_clk                                => clk100,
+                clk_125_clk                                => clk125,
                 reset_reset_n                              => reset_n,
                 pcie_ip_refclk_export                      => PCIE_REFCLK_P,
                 pcie_ip_pcie_rstn_export                   => PCIE_PERST_N,
                 pcie_ip_rx_in_rx_datain_0                  => PCIE_RX_P(0),
                 pcie_ip_tx_out_tx_dataout_0                => PCIE_TX_P(0),
+                pcie_ip_reconfig_fromgxb_0_data            => open,
+                pcie_ip_clocks_sim_clk250_export           => open,
+                pcie_ip_clocks_sim_clk500_export           => open,
+                pcie_ip_clocks_sim_clk125_export           => open,
+                pcie_ip_reconfig_busy_busy_altgxb_reconfig => open,
+                pcie_ip_pipe_ext_pipe_mode                 => cInactivated,
+                pcie_ip_pipe_ext_phystatus_ext             => cInactivated,
+                pcie_ip_pipe_ext_rate_ext                  => open,
+                pcie_ip_pipe_ext_powerdown_ext             => open,
+                pcie_ip_pipe_ext_txdetectrx_ext            => open,
+                pcie_ip_pipe_ext_rxelecidle0_ext           => cInactivated,
+                pcie_ip_pipe_ext_rxdata0_ext               => (others => cInactivated),
+                pcie_ip_pipe_ext_rxstatus0_ext             => (others => cInactivated),
+                pcie_ip_pipe_ext_rxvalid0_ext              => cInactivated,
+                pcie_ip_pipe_ext_rxdatak0_ext              => cInactivated,
+                pcie_ip_pipe_ext_txdata0_ext               => open,
+                pcie_ip_pipe_ext_txdatak0_ext              => open,
+                pcie_ip_pipe_ext_rxpolarity0_ext           => open,
+                pcie_ip_pipe_ext_txcompl0_ext              => open,
+                pcie_ip_pipe_ext_txelecidle0_ext           => open,
+                pcie_ip_reconfig_togxb_data                => open,
+                pcp_0_cpu_resetrequest_resetrequest        => '0',
+                pcp_0_cpu_resetrequest_resettaken          => open,
+                pcie_ip_powerdown_pll_powerdown            => cInactivated,
+                pcie_ip_powerdown_gxb_powerdown            => cInactivated,
                 led_external_connection_export             => ledRG(20 downto 0),
                 button_external_connection_export          => KEY,
---                sdram_addr                                 => DRAM_ADDR,
---                sdram_ba                                   => DRAM_BA,
---                sdram_cas_n                                => DRAM_CAS_N,
---                sdram_cke                                  => DRAM_CKE,
---                sdram_cs_n                                 => DRAM_CS_N,
---                sdram_dq                                   => DRAM_DQ,
---                sdram_dqm                                  => DRAM_DQM,
---                sdram_ras_n                                => DRAM_RAS_N,
---                sdram_we_n                                 => DRAM_WE_N,
-               -- tristate_conduit_bridge_0_out_SSRAM_1_CS_N => SSRAM1_CE_N,
-                tristate_conduit_bridge_0_out_DATA         => FS_DQ,
-                tristate_conduit_bridge_0_out_SSRAM_BE_N   => SSRAM_BE,
-                --tristate_conduit_bridge_0_out_CFI_0_WR_N   => FL_WE_N,
-                tristate_conduit_bridge_0_out_ADDR         => FS_ADDR,
-                tristate_conduit_bridge_0_out_SSRAM_BT_N   => SSRAM_ADSC_N,
-                tristate_conduit_bridge_0_out_SSRAM_WR_N   => SSRAM_WE_N,
-                --tristate_conduit_bridge_0_out_CFI_0_OE_N   => FL_OE_N,
-                tristate_conduit_bridge_0_out_SSRAM_0_CS_N => SSRAM0_CE_N,
-                --tristate_conduit_bridge_0_out_CFI_0_CS_N   => FL_CE_N,
-                tristate_conduit_bridge_0_out_SSRAM_OE_N   => SSRAM_OE_N,
-                --tristate_conduit_bridge_0_out_CFI_0_RST_N  => flash_reset_n,
-                flash_reset_n_export                       => nios_flash_reset_n,
+                tri_state_0_tcm_data_out                   => FS_DQ,
+                tri_state_0_tcm_byteenable_n_out           => SSRAM_BE,
+                tri_state_0_tcm_address_out                => FS_ADDR,
+                tri_state_0_tcm_begintransfer_n_out        => SSRAM_ADSC_N,
+                tri_state_0_tcm_write_n_out                => SSRAM_WE_N,
+                tri_state_0_tcm_chipselect_n_out           => SSRAM0_CE_N,
+                tri_state_0_tcm_outputenable_n_out         => SSRAM_OE_N,
                 openmac_mii_txEnable                       => ENET_TX_EN,
                 openmac_mii_txData                         => ENET_TX_DATA,
                 openmac_mii_txClk                          => ENET_TX_CLK,
@@ -446,25 +380,16 @@ begin
                 openmac_smi_clk                            => ENET_MDC,
                 openmac_smi_dio                            => ENET_MDIO,
                 benchmark_pio_export                       => ledRG_b( 7 downto 0)
-					);
+                );
 
-    --TODO: Remove this block - Just for checking FPGA design
-    heart_beat_clk50: component heart_beat
-        port map (
-            clk     => clk50,
-            led     => hb_50
-          );
-
-    --! PLL Instantiation
+   --! PLL Instantiation
     PLL1: component pll
         port map (
            inclk0   => CLOCK_50,
-           c0     => clk50,
-           c1     => clk100,
-           c2     => clk25,
-           c3     => clk150p,
-           c4     => clk150,
-           locked => pllLocked
+           c0       => clk50,
+           c1       => clk100,
+           c2       => clk125,
+           locked   => pllLocked
         );
 
 end rtl;
