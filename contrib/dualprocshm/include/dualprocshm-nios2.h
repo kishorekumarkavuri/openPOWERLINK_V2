e398b33a3aaadc3785fd6baf52d991801d513aa4
Kishore Kumar <kishorekumar.k@kalycito.com>
2015-03-25 10:59:33 +0530

    [TASK] Update PCIe subsystem Qsys project
    - Added top level parameters.
    - Update parameter names for NIOS2 processor in dualprocshm library.
    
    Signed-off-by: Kishore Kumar <kishorekumar.k@kalycito.com>

--- contrib/dualprocshm/include/dualprocshm-nios2.h
+++ contrib/dualprocshm/include/dualprocshm-nios2.h
@@ -95,9 +95,9 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #define DPSHM_ENABLE_INTR(fEnable)  target_enableGlobalInterrupt(fEnable)
 
 #define DPSHM_ENABLE_HOST_SYNC_IRQ()    \
-                    DPSHM_WRITE8(PCIE_HARD_IP_0_BASE + 0x50, 0x1)
+                    DPSHM_WRITE8(PCIE_CONFIG_BASE + 0x50, 0x1)
 #define DPSHM_DISABLE_HOST_SYNC_IRQ()   \
-                    DPSHM_WRITE8(PCIE_HARD_IP_0_BASE + 0x50, 0x0)
+                    DPSHM_WRITE8(PCIE_CONFIG_BASE + 0x50, 0x0)
 
 // Memory barrier
 // FIXME: Find other suitable way to handle memory barrier for NIOS2
